f4c463828329bb61ce0eefff0a1720741d5a266e
Stoyan Hristov <stoqnhristov93@gmail.com>
2023-08-20 14:36:14 +0300

    Changed Grid2D
    It will be a square now so we can guarantee equal distance between different node

--- js/Grid2D.js
+++ js/Grid2D.js
@@ -2,15 +2,13 @@ class Grid2d {
     static divisions = 100;
 
     constructor(maxX, maxY) {
-        this.gridSizeX = Math.ceil(maxX);
-        this.gridSizeY = Math.ceil(maxY);
-        this.gridDensityX = this.gridSizeX / Grid2d.divisions;
-        this.gridDensityY = this.gridSizeY / Grid2d.divisions;
+        this.gridSize = Math.ceil(maxX) >  Math.ceil(maxY) ?  Math.ceil(maxX) :  Math.ceil(maxY);
+        this.gridDensity = this.gridSize / Grid2d.divisions;
         this.grid = [];
         for (let i = 0; i < Grid2d.divisions ; ++i) {
             this.grid.push([]);
             for (let j = 0; j < Grid2d.divisions; ++j) {
-                this.grid[i].push(new THREE.Vector3(i * this.gridDensityX, j * this.gridDensityY, 0));
+                this.grid[i].push(new THREE.Vector3(i * this.gridDensity, j * this.gridDensity, 0));
             }
         }
     }
